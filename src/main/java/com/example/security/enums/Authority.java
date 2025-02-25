0afb6bf0cb96c642a8e018b4f465bb1ed69a34c6
walidmadad <walid.madad123@gmail.com>
2025-02-25 17:37:02 +0100

    add Authorities

---
+++ src/main/java/com/example/security/enums/Authority.java
@@ -0,0 +1,18 @@
+package com.example.security.enums;
+
+import static com.example.security.constant.Constants.*;
+
+public enum Authority {
+    USER(USER_AUTHORITIES),
+    ADMIN(ADMIN_AUTHORITIES),
+    SUPER_ADMIN(SUPER_ADMIN_AUTHORITIES),
+    MANAGER(MANAGER_AUTHORITIES);
+
+    private final String value;
+    Authority(String value) {
+        this.value = value;
+    }
+    public String getValue() {
+        return this.value;
+    }
+}
