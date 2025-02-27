96c2765095f216cd47ba4ec74a23209a065409f4
walidmadad <walid.madad123@gmail.com>
2025-02-27 12:22:50 +0100

    add EmailService Interface

---
+++ src/main/java/com/example/security/service/EmailService.java
@@ -0,0 +1,6 @@
+package com.example.security.service;
+
+public interface EmailService {
+    void sendNewAccountEmail(String name, String to, String token);
+    void sendPasswordResetEmail(String name, String to, String token);
+}
